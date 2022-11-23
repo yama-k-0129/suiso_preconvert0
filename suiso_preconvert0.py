70a45ac8f79ce76070d6bfb4bcb388f9f5fb5d94
yama-k-0129 <110022751+yama-k-0129@users.noreply.github.com>
2022-11-23 16:31:23 +0900

    Update suiso_preconvert0.py

--- suiso_preconvert0.py
+++ suiso_preconvert0.py
@@ -23,7 +23,23 @@ if uploaded_files:
         def convert_df(df):
             # IMPORTANT: Cache the conversion to prevent computation on every rerun
             return df.to_csv(index=None).encode('utf-8')
-        radians = ['0', '22.5', '45','67.5','90','112.5','135','157.5','180','202.5','225','247.5','270','292.5','315','337.5','360']
+        radians = list(range(0, 382.5, 22.5))
+        for radian in radians:
+            df2['radian'] = radian
+            csv = convert_df(df2) 
+            fname = 'ダウンロード' + str(radian)
+            fname2 = 'suiso' + str(radian) + str('.csv')
+            st.download_button(
+                fname,data = csv,file_name=fname2,
+                mime = 'text/csv'
+            )
+    elif radian_menu == '32':
+        df2 = pd.concat([df, df1], sort=False)
+        df2['distance'] = 1000
+        def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv(index=None).encode('utf-8')
+        radians = list(range(0,370,10))
         for radian in radians:
             df2['radian'] = radian
             csv = convert_df(df2) 
